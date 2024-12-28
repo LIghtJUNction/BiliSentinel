b69b833b0f0ce23796f09012e22bd385ff1c0929
LIghtJUNction <106986785+LIghtJUNction@users.noreply.github.com>
2024-12-29 02:36:44 +0800

    Update check_lowlevel.sh

--- check_lowlevel.sh
+++ check_lowlevel.sh
@@ -7,6 +7,12 @@ end_uid=$2
 # Lowlevel名单文件
 lowlevel_file="Lowlevel.txt"
 
+# 检查 Lowlevel.txt 是否存在，不存在则创建并设置可写权限
+if [ ! -f "$lowlevel_file" ]; then
+    touch "$lowlevel_file"
+    chmod 666 "$lowlevel_file"  # 设置文件为可写
+fi
+
 # 遍历 UID 区间
 for ((uid=$start_uid; uid<$end_uid; uid++))
 do
@@ -15,7 +21,7 @@ do
 import sys
 from Collect import extract_bilibili_data
 uid = $uid
-user_info = extract_bilibili_data(uid,24)
+user_info = extract_bilibili_data(uid, 24)
 if user_info:
     print(user_info)
 else:
@@ -25,8 +31,8 @@ else:
     # 检查提取的数据是否为空
     if [[ "$user_data" != "None" ]]; then
         # 解析出用户的等级和简介
-        level=$($user_data[0])
-        sign=$($user_data[1])
+        level=$(echo $user_data | cut -d',' -f1 | tr -d '[] "')
+        sign=$(echo $user_data | cut -d',' -f4 | tr -d '[] "')
 
         # 如果等级 <= 1 且简介为空，则写入 Lowlevel.txt 文件
         if [ "$level" -le 1 ] && [ -z "$sign" ]; then
